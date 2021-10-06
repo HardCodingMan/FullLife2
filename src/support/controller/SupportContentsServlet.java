2496b3006ef7b812f98bb0e4d2efa7d829a22d06
hardcodingman <xjzlfhrkwk@gmail.com>
2021-10-06 19:32:09 +0900

    게시글, 댓글, 작성 시 포인트 지급

--- src/support/controller/SupportContentsServlet.java
+++ src/support/controller/SupportContentsServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 
 import apply.model.service.ApplyNoticeService;
 import apply.model.vo.Notice;
+import member.model.vo.Member;
 
 /**
  * Servlet implementation class SupportContentsServlet
@@ -35,8 +36,10 @@ public class SupportContentsServlet extends HttpServlet {
 		String userId = (String)session.getAttribute("userId");
 		int noticeNo = Integer.parseInt(request.getParameter("noticeNo"));
 		Notice sOne = new ApplyNoticeService().printOneBySupportNo(noticeNo);
+		Member point = new ApplyNoticeService().getPoint(userId); 
 		int sOnePercentage = (int)((double)sOne.getNowSupport() / (double)sOne.getNeedSupport() * 100);
 		if(sOne != null) {
+			request.setAttribute("point", point);
 			request.setAttribute("percentage", sOnePercentage);
 			request.setAttribute("userId", userId);
 			request.setAttribute("sOne", sOne);
